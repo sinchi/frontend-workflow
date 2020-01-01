6f1964bcce768c5e49f0b07bae8883757497c0d6
ayoub <ayoub.belghar@gmail.com>
2020-01-01 14:04:37 +0000

    fix async test with moxios

--- src/containers/CommentContainer/actions.js
+++ src/containers/CommentContainer/actions.js
@@ -11,7 +11,7 @@ export const addComment = (comment) => {
 export const fetchComments = async () => {
     let comments;
     try {
-        comments= await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments');
+        comments= await axios.get('https://jsonplaceholder.typicode.com/comments');
         return {
             type: FETCH_COMMENTS_SUCCESS,
             comments
